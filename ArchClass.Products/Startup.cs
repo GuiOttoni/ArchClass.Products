9de4a804fc0ebe1ebf76c96a6bd88906ccf0426d
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2021-12-13 23:15:13 -0300

    !!!swagger in production!!!

--- ArchClass.Products/Startup.cs
+++ ArchClass.Products/Startup.cs
@@ -43,9 +43,10 @@ namespace ArchClass.Products
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ArchClass.Products v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ArchClass.Products v1"));
 
             app.UseHttpsRedirection();
 
